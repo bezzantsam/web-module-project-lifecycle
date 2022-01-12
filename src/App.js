b8511d134ca86f7283e72a2b171948fff42d16f4
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-01-12 02:23:43 -0800

    jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return (
+      <div>
+        <h1>GITHUB INFO</h1>
+        <form>
+          <input placeholder="Github Handle"></input>
+          <button>Search</button>
+        </form>
+        <div id="UserCard">
+          <img src="https://avatars.githubusercontent.com/u/83626945?v=4" />
+          <a target="blank" href="https://github.com/bezzantsam">
+            <h3>Sam Bezzant</h3>
+            <p> (bezzantsam) </p>
+            <p> Total Repositories: 70</p>
+            <p>Total Followers: 15</p>
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
